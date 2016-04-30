b9166ce44f8dce78eabf8be05eccc94bec38d005
mizuki-y <mizuki-y@syati.info>
2016-04-30 18:07:30 +0900

    Move app

--- app/common/containers/App/App.js
+++ app/common/containers/App/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import Header from '../components/Header/Header';
-import Footer from '../components/Footer/Footer';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 
 class App extends React.Component {
   render() {
     const { children } = this.props;
+    const styles = require('./App.less');
 
     return (
       <div>
